43217d20c4ec33392565a0bfea2d8067f20e6c2a
hamsame <hamsaaelmi@gmail.com>
2021-10-12 22:43:44 +0100

    fixed error

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import Navbar from './Navbar'
+import Navbar from './navbar'
 import Footer from './Footer'
 
 const Layout = ({ children }) => {
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Navbar from '../components/Navbar'
+
 export default function Home() {
   return (
     <div style={{ height: '300vh', position: 'relative', width: 'initial' }}>
@@ -9,7 +9,6 @@ export default function Home() {
         <meta name='description' content='Gamerland- The place for gamers' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Navbar />
     </div>
   )
 }
